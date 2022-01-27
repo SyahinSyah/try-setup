7135a9f67eca89e65353c17f6b5b2a3f6bf158b7
SyahinSyah <syahinsyah7@gmail.com>
2022-01-28 01:03:15 +0800

    bug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 // use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 // use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -40,5 +41,36 @@ class AuthController extends Controller
             'user' => $user,
             'token' => $token
         ]);
+    }
+
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exists:users,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false ;
+        unset($credentials['remember']);
+
+        if(!Auth::attempt([$credentials,$remember]))
+        {
+            return response([
+                'error' => 'The provided credentials are not correct'
+            ], 422);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+
     }
 }
